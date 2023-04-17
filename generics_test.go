9d22580cdf3c8aa1c1fb3c0c11fe146c2e754acd
Aleksandr Razumov <ernado@ya.ru>
2023-04-17 10:37:44 +0300

    feat: Wrapped use Opt directly

--- generics_test.go
+++ generics_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Wrapped struct {
-	Str OptStr
+	Str Opt[string]
 }
 
 func TestOptStr(t *testing.T) {
@@ -33,22 +33,34 @@ func TestOptStr(t *testing.T) {
 }
 
 func TestWrapped(t *testing.T) {
-	var w Wrapped
-	assert.False(t, w.Str.IsSet(), "expected not set")
-	assert.False(t, w.Str.Set, "expected not set")
-	assert.Equal(t, "", w.Str.Value, "expected empty string")
+	t.Run("Set", func(t *testing.T) {
+		var w Wrapped
 
-	w.Str.SetTo("foo")
-	assert.True(t, w.Str.Set, "expected set")
-	assert.Equal(t, "foo", w.Str.Value, "expected value")
-	assert.True(t, w.Str.IsSet(), "expected set")
-	if v, ok := w.Str.Get(); !ok || v != "foo" {
-		t.Fatalf("expected foo, got %v", v)
-	}
-	assert.Equal(t, "foo", w.Str.Or("bar"), "Or")
+		var v OptStr
+		v.SetTo("foo")
 
-	w.Str.Reset()
-	assert.False(t, w.Str.IsSet(), "expected not set")
-	assert.False(t, w.Str.Set, "expected not set")
-	assert.Equal(t, "", w.Str.Value, "expected empty string")
+		w.Str = v
+
+		assert.True(t, w.Str.Set, "expected set")
+	})
+	t.Run("Positive", func(t *testing.T) {
+		var w Wrapped
+		assert.False(t, w.Str.IsSet(), "expected not set")
+		assert.False(t, w.Str.Set, "expected not set")
+		assert.Equal(t, "", w.Str.Value, "expected empty string")
+
+		w.Str.SetTo("foo")
+		assert.True(t, w.Str.Set, "expected set")
+		assert.Equal(t, "foo", w.Str.Value, "expected value")
+		assert.True(t, w.Str.IsSet(), "expected set")
+		if v, ok := w.Str.Get(); !ok || v != "foo" {
+			t.Fatalf("expected foo, got %v", v)
+		}
+		assert.Equal(t, "foo", w.Str.Or("bar"), "Or")
+
+		w.Str.Reset()
+		assert.False(t, w.Str.IsSet(), "expected not set")
+		assert.False(t, w.Str.Set, "expected not set")
+		assert.Equal(t, "", w.Str.Value, "expected empty string")
+	})
 }
